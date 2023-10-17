f6d22c12730fc6381cb4074266cf7d47972e3ed2
Joana <joana.barbermo@elorrieta-errekamari.com>
2023-10-17 18:47:28 +0200

    first commit

---
+++ src/main/java/com/reto1/myTube/MyTubeApplication.java
@@ -0,0 +1,13 @@
+package com.reto1.myTube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTubeApplication.class, args);
+	}
+
+}
